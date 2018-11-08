7d16be50f57b345722289ef4366f46c06f8b44a6
Brian Tambara <btambara@gmail.com>
2018-11-08 14:40:59 -0800

    Initial

---
+++ src/main/java/com/app/tambara/homeduty/HomeDutyApplication.java
@@ -0,0 +1,12 @@
+package com.app.tambara.homeduty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeDutyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeDutyApplication.class, args);
+	}
+}
